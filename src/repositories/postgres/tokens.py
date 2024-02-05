8abe09e0c786c1924cfbf566036f775668b50d88
Sergey Udachin <s.udachin@mail.ru>
2024-02-05 14:59:26 +0300

    chore: cleanup

--- src/repositories/postgres/tokens.py
+++ src/repositories/postgres/tokens.py
@@ -1,5 +1,3 @@
-import logging
-
 import asyncpg
 
 
@@ -9,13 +7,6 @@ class TokensRepository:
 
     async def does_token_exist(self, token: str) -> bool:
         return await self._update_token_usage(token)
-        #TODO: remove
-
-        try:
-            await self._update_token_usage(token)
-            return True
-        except asyncpg.PostgresError:
-            return False
 
     async def _update_token_usage(self, token: str) -> bool:
         query = """
@@ -24,7 +15,7 @@ class TokensRepository:
             SET
                 request_count = request_count + 1,
                 updated_at = now()
-            WHERE 
+            WHERE
                 api_key = $1
             RETURNING api_key;
         """
@@ -32,26 +23,3 @@ class TokensRepository:
             rows = await connection.fetch(query, token)
         is_updated = bool(rows)
         return is_updated
-
-        #TODO: remove
-
-        try:
-            async with self.pool.acquire() as connection:
-                await connection.execute(query, token)
-            return True
-        except asyncpg.PostgresError as e:
-            raise e
-
-    # TODO: remove if not used
-    async def add_new_token(self, new_token: str) -> None:
-        query = """
-                    INSERT
-                    INTO tokens (api_key)
-                    VALUES ($1);
-                    """
-
-        async with self.pool.acquire() as connection:
-            await connection.execute(
-                query,
-                new_token
-            )

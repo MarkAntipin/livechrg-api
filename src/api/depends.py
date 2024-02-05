45c35c3997839f2a8552d06f44bd719629064e32
Sergey Udachin <s.udachin@mail.ru>
2024-02-05 14:56:32 +0300

    feat: 401 or 403 for failing API KEY

--- src/api/depends.py
+++ src/api/depends.py
@@ -1,10 +1,11 @@
-from fastapi import Request, HTTPException, Security, status, Depends
+
+from fastapi import Depends, HTTPException, Request, Security, status
 from fastapi.security import APIKeyHeader
 
 from src.services.stations import StationsServices
 from src.services.tokens import TokenServices
 
-API_KEY_HEADER = APIKeyHeader(name="api-key")
+API_KEY_HEADER = APIKeyHeader(name="api-key", auto_error=False)
 
 
 async def get_stations_service(request: Request) -> StationsServices:
@@ -19,7 +20,8 @@ async def check_api_key(
         api_key_header: str = Security(API_KEY_HEADER),
         token_service: TokenServices = Depends(get_token_service)
 ) -> str:
-    # TODO: 401 if no token passed
+    if api_key_header is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="API KEY is missing")
     if await token_service.does_token_exist(api_key_header):
         return api_key_header
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="API KEY is invalid or missing")
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="API KEY is invalid")

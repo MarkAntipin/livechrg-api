605c1fdb30622c6c52a3bf1f091bbfbd841099e1
Sergey Udachin <s.udachin@mail.ru>
2024-02-05 14:55:22 +0300

    feat:  field is TEXT

--- migrations/20240124181421_create_tokens_table.up.sql
+++ migrations/20240124181421_create_tokens_table.up.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS tokens (
     id SERIAL PRIMARY KEY,
---     # TODO: TEXT
-    api_key VARCHAR(100) NOT NULL,
+    api_key TEXT NOT NULL,
     request_count INTEGER NOT NULL DEFAULT 0,
---     # TODO: CURRENT_TIMESTAMP
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
 

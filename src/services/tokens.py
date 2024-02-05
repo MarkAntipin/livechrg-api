2633370db02c65e1e529b6d1a360da5535c108f1
Sergey Udachin <s.udachin@mail.ru>
2024-02-05 14:57:33 +0300

    chore: TODOs and cleanup

--- src/services/tokens.py
+++ src/services/tokens.py
@@ -1,4 +1,3 @@
-import uuid
 
 import asyncpg
 
@@ -9,22 +8,5 @@ class TokenServices:
     def __init__(self, pool: asyncpg.Pool) -> None:
         self._tokens_repo = TokensRepository(pool=pool)
 
-    # TODO: remove if not used
-    async def add_new_token(self) -> str:
-        new_token = str(uuid.uuid4())
-        await self._tokens_repo.add_new_token(new_token)
-        return new_token
-
     async def does_token_exist(self, token: str) -> bool:
-        # TODO: without uuid validation
-        if await self._is_valid_uuid(token):
-            return await self._tokens_repo.does_token_exist(token)
-        return False
-
-    @staticmethod
-    async def _is_valid_uuid(uuid_to_test: any, version: int = 4) -> bool:
-        try:
-            uuid_obj = uuid.UUID(uuid_to_test, version=version)
-        except ValueError:
-            return False
-        return str(uuid_obj) == uuid_to_test
+        return await self._tokens_repo.does_token_exist(token)

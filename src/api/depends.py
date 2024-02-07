a140a8f9e027009a1c5b3bb19eaef1611e0e3269
Sergey Udachin <s.udachin@mail.ru>
2024-02-07 12:03:28 +0300

    chore: remove unused statements

--- src/api/depends.py
+++ src/api/depends.py
@@ -1,10 +1,7 @@
 from fastapi import Request
-from fastapi.security import APIKeyHeader
 
 from src.services.stations import StationsServices
 from src.services.tokens import TokenServices
-
-API_KEY_HEADER = APIKeyHeader(name="api-key", auto_error=False)
 
 
 async def get_stations_service(request: Request) -> StationsServices:

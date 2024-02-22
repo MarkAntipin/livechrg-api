cfdd1943ff2857d53aa9db1acf04e2ed38c98584
Ilia Munaev <ilyamunaev@gmail.com>
2024-02-22 17:33:35 +0200

    Create endpoint

--- src/api/routers/inner/stations.py
+++ src/api/routers/inner/stations.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, Response, status
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from fastapi.security import APIKeyHeader
 
 from src.api.depends import get_stations_service
-from src.api.routers.inner.models import AddStationsRequest
+from src.api.routers.inner.models import AddStationsRequest, StationSources
 from src.api.security import check_authorization_header
 from src.services.stations import StationsServices
 
@@ -17,3 +19,17 @@ async def add_stations(
 ) -> Response:
     await stations_service.add_stations(stations=request.stations)
     return Response(status_code=status.HTTP_201_CREATED)
+
+
+@router.get('sources/', include_in_schema=False)
+async def get_station_inner_id_and_source_by_station_id(
+        station_ids: List[int],
+        stations_service: StationsServices = Depends(get_stations_service),
+        _: APIKeyHeader = Depends(check_authorization_header)
+) -> list[StationSources]:
+    station_sources = await stations_service.get_station_inner_id_and_source_by_station_id(
+        station_ids=station_ids)
+    if not station_sources:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    return station_sources

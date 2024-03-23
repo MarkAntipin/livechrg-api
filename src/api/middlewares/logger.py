651ac017661f14f71bb9b15965207345888bec96
Andrey Rahimov <rahimov.work@gmail.com>
2024-03-23 11:00:08 +0500

    refactor: refactor logger formatter

--- src/api/middlewares/logger.py
+++ src/api/middlewares/logger.py
@@ -1,10 +1,27 @@
+from datetime import datetime
+import json
 import logging
 
 from fastapi import Request
 from starlette.responses import Response
 
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+
+        log = {
+            "timestamp": datetime.fromtimestamp(record.created).isoformat(),
+        }
+
+        if isinstance(msg := record.msg, dict):
+            log |= msg
+        # if record.exc_info:
+        #     log_message["exception"] = self.formatException(record.exc_info)
+        return json.dumps(log)
+
+
 logger = logging.getLogger(__name__)
-formatter = logging.Formatter('[%(asctime)s] - #%(levelname)s - %(message)s')
+formatter = JsonFormatter()
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
@@ -18,7 +35,7 @@ async def logger_middleware(request: Request, call_next: callable) -> Response:
         'log_type': 'request',
         'method': request.method,
         'url': request.url.path,
-        'query-params': request.query_params,
+        'query-params': dict(request.query_params),
         'path_params': request.path_params,
     }
     logger.info(request_log)

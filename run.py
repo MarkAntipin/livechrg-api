987fb3b287ad3ac3d6f42e53bfc0b54fb0802824
Andrey Rahimov <rahimov.work@gmail.com>
2024-03-24 09:37:42 +0500

    refactor: move adding logger_middleware into function create_app

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 import uvicorn
-from starlette.middleware.base import BaseHTTPMiddleware
 
 from settings import AppSettings
 from src.api.app import create_app
-from src.api.middlewares.logger import logger_middleware
 
 app_settings = AppSettings()
 app = create_app(settings=app_settings)
-app.add_middleware(BaseHTTPMiddleware, dispatch=logger_middleware)
 
 if __name__ == "__main__":
     uvicorn.run(

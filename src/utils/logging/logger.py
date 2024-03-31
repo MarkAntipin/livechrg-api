28aa348bdae8073b04b8b2396e86f9f91382326f
Andrey Rahimov <rahimov.work@gmail.com>
2024-03-31 14:44:45 +0500

    refactor: refactor logger

--- src/utils/logging/logger.py
+++ src/utils/logging/logger.py
@@ -1,3 +1,7 @@
+import sys
+
 from loguru import logger
 
-logger.debug("That's it, beautiful and simple logging!")
+logger.remove()
+logger.add(sys.stdout, serialize=True)
+logger.info("That's it, beautiful and simple logging!")
